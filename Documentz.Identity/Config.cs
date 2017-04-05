d633dde15dbdeb29af00d1bbfd347785d3670a53
Arkadiusz Szechlicki <arkadiusz.szechlicki@mobica.com>
2017-04-06 00:16:36 +0200

    inistial commit for identity

---
+++ Documentz.Identity/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Documentz.Identity
+{
+    public class Config
+    {
+        public static List<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "My Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "Documentz",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("DocumentzSecret".Sha256())
+                    },
+                    AllowedScopes = {"api"}
+                }
+            };
+        }
+    }
+}
